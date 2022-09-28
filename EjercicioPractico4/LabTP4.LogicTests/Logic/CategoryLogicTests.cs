ced30c1679eed7ed12aeaf7a38c7c5aa3ed21d44
MarcosMM13 <97316968+MarcosMM13@users.noreply.github.com>
2022-09-27 23:13:50 -0300

    Unit test

---
+++ EjercicioPractico4/LabTP4.LogicTests/Logic/CategoryLogicTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabTP4.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabTP4.Entities;
+
+namespace LabTP4.Logic.Tests
+{
+    [TestClass()]
+    public class CategoryLogicTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            List<Category> category = new List<Category>();
+            CategoryLogic categoryLogic = new CategoryLogic();
+
+            List<Category> expected = category.ToList();
+            List<Category> actual = categoryLogic.GetAll();
+            CollectionAssert.AreEquivalent(expected, actual);
+           
+        }
+    }
+}
